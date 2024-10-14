58f7584b674218cf72bc844fc29dc53ecb3b8230
Nicholas Rubin <nickrubin@google.com>
2024-10-14 18:46:26 +0000

    working for undefined mean-field

--- qcpanop/cc/projective_cc/custom_ucc/test_ucc.py
+++ qcpanop/cc/projective_cc/custom_ucc/test_ucc.py
@@ -24,8 +24,7 @@ def test_uccsd_vs_rccsd():
     chem_eri = ao2mo.general(
         mol, (orb, orb, orb, orb), compact=False).reshape(nmo, nmo, nmo, nmo)
 
-    mf = mf.to_uhf()
-    out, out_t = run_ucc(hcore, hcore, chem_eri, chem_eri, chem_eri, nalpha, nbeta, mf, True)
+    out, out_t = run_ucc(hcore, hcore, chem_eri, chem_eri, chem_eri, nalpha, nbeta, mol, True)
     assert abs(ref - out) < 1e-6
     assert abs(ref_t - out_t) < 1e-6
 
@@ -54,11 +53,85 @@ def test_uccsd_vs_uccsd():
     chem_eriab = ao2mo.general(
         mol, (moa, moa, mob, mob), compact=False).reshape(nmo, nmo, nmo, nmo)
 
-    out, out_t = run_ucc(ha, hb, chem_eriaa, chem_eribb, chem_eriab, nalpha, nbeta, mf, True)
+    out, out_t = run_ucc(ha, hb, chem_eriaa, chem_eribb, chem_eriab, nalpha, nbeta, mol, True)
+    assert abs(ref - out) < 1e-6
+    assert abs(ref_t - out_t) < 1e-6
+
+
+def test_fake_mol():
+    mol = pyscf.M(
+        atom='H 0 0 0; F 0 0 1.1',
+        basis='ccpvdz', charge=1, spin=1)
+
+    mol2 = pyscf.M(
+        atom='H 0 0 0; F 0 0 1.1',
+        basis='sto-3g', charge=1, spin=1)
+
+    nalpha, nbeta = mol.nelec
+    mf = mol.UHF()
+    mf.kernel()
+    cc = UCCSD(mf)
+    ref = cc.kernel()[0]
+    ref_t = cc.ccsd_t()
+
+    moa = mf.mo_coeff[0]
+    mob = mf.mo_coeff[1]
+    ha = reduce(numpy.dot, (moa.T, mf.get_hcore(), moa))
+    hb = reduce(numpy.dot, (mob.T, mf.get_hcore(), mob))
+    nmo = moa.shape[1]
+    assert mob.shape[1] == nmo
+    chem_eriaa = ao2mo.general(
+        mol, (moa, moa, moa, moa), compact=False).reshape(nmo, nmo, nmo, nmo)
+    chem_eribb = ao2mo.general(
+        mol, (mob, mob, mob, mob), compact=False).reshape(nmo, nmo, nmo, nmo)
+    chem_eriab = ao2mo.general(
+        mol, (moa, moa, mob, mob), compact=False).reshape(nmo, nmo, nmo, nmo)
+
+    out, out_t = run_ucc(ha, hb, chem_eriaa, chem_eribb, chem_eriab, nalpha, nbeta, mol2, True)
+    assert abs(ref - out) < 1e-6
+    assert abs(ref_t - out_t) < 1e-6
+
+def test_fake_mol2():
+    mol = pyscf.M(
+        atom='H 0 0 0; F 0 0 1.1',
+        basis='ccpvdz', charge=1, spin=1)
+
+    mol2 = pyscf.M(
+        atom='H 0 0 0; F 0 0 1.1',
+        basis='sto-3g', charge=1, spin=1)
+
+    mol2_ = pyscf.M()
+    mol2_.nelectron = mol2.nelectron
+    mol2_.spin = mol2.spin
+    mol2_.charge = mol2.charge
+
+    nalpha, nbeta = mol.nelec
+    mf = mol.UHF()
+    mf.kernel()
+    cc = UCCSD(mf)
+    ref = cc.kernel()[0]
+    ref_t = cc.ccsd_t()
+
+    moa = mf.mo_coeff[0]
+    mob = mf.mo_coeff[1]
+    ha = reduce(numpy.dot, (moa.T, mf.get_hcore(), moa))
+    hb = reduce(numpy.dot, (mob.T, mf.get_hcore(), mob))
+    nmo = moa.shape[1]
+    assert mob.shape[1] == nmo
+    chem_eriaa = ao2mo.general(
+        mol, (moa, moa, moa, moa), compact=False).reshape(nmo, nmo, nmo, nmo)
+    chem_eribb = ao2mo.general(
+        mol, (mob, mob, mob, mob), compact=False).reshape(nmo, nmo, nmo, nmo)
+    chem_eriab = ao2mo.general(
+        mol, (moa, moa, mob, mob), compact=False).reshape(nmo, nmo, nmo, nmo)
+
+    out, out_t = run_ucc(ha, hb, chem_eriaa, chem_eribb, chem_eriab, nalpha, nbeta, mol2_, True)
     assert abs(ref - out) < 1e-6
     assert abs(ref_t - out_t) < 1e-6
 
 
 if __name__ == '__main__':
-    test_uccsd_vs_rccsd()
-    test_uccsd_vs_uccsd()
+    # test_uccsd_vs_rccsd()
+    # test_uccsd_vs_uccsd()
+    # test_fake_mol()
+    test_fake_mol2()
--- qcpanop/cc/projective_cc/custom_ucc/ucc.py
+++ qcpanop/cc/projective_cc/custom_ucc/ucc.py
@@ -15,14 +15,38 @@ def run_ucc(
     eri_ab: numpy.ndarray,
     nalpha: int,
     nbeta: int,
-    mf: pyscf.scf.hf,
+    #mf: pyscf.scf.hf,
+    mol: pyscf.gto.Mole,
     with_triples: Optional[bool] = False,
 ):
+    """
+    Note, in order for this to work you need to set up a Mol object
+    that has the minimum amount of information to determine the 
+    number of alpha-electron and number of beta-electron. 
 
+    For example, you need to set nelectron, spin and charge fields
+
+    mol2_ = pyscf.M()
+    mol2_.nelectron = A
+    mol2_.spin = B
+    mol2_.charge = C
+
+    so that the nalpha, nbeta can be determined properly.
+    """
+    nmo = h1a.shape[0]
+    mf = mol.UHF()
+    mf.mo_coeff = (numpy.identity(nmo), numpy.identity(nmo))
+    mf.mo_occ = numpy.asarray([
+        [1]*nalpha + [0]*(nmo - nalpha),
+        [1]*nbeta + [0]*(nmo - nbeta),
+    ])
+    mf.make_rdm1 = lambda *args: numpy.zeros((2, nmo, nmo))
+    mf.get_veff = lambda *args: numpy.zeros((2, nmo, nmo))
+    mf.energy_tot = lambda *args, **kwargs: 0
     ucc = UCCSD(mf)
-    eris = CustomUERIs(h1a, h1b, eri_aa, eri_bb, eri_ab, nalpha, nbeta, mf.mol)
+    eris = CustomUERIs(h1a, h1b, eri_aa, eri_bb, eri_ab, nalpha, nbeta, mol)
     ucc.kernel(eris=eris)
     et = None
     if with_triples:
-        et = ucc.ccsd_t()
+        et = ucc.ccsd_t(eris=eris)
     return ucc.e_corr, et
